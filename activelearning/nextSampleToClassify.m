1e6a382284d50da37eef08088b405721f809f837
Henry Pinkard <henry.pinkard@gmail.com>
2016-08-01 00:08:24 -0400

    Begin adding active learning master controls

---
+++ activelearning/nextSampleToClassify.m
@@ -0,0 +1,11 @@
+function [ index ] = nextSampleToClassify( features, labelledTCell, labelledNotTCell )
+%NEXTSAMPLETOCLASSIFY 
+%Use the active learning query strategy to determine which unlabelled
+%instance should be classified next, and return its index within the
+%nxp data matrix features
+
+
+
+%index should be 1-based
+end
+
