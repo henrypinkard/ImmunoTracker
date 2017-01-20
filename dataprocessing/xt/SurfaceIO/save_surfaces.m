e58cb5df0fc368bdd919394f4b3e490ae86ecd0e
Henry Pinkard <henry.pinkard@gmail.com>
2017-01-19 16:23:56 -0800

    reorganized folders

--- dataprocessing/xt/SurfaceIO/save_surfaces.m
+++ dataprocessing/xt/SurfaceIO/save_surfaces.m
@@ -59,3 +59,27 @@ copyfile(saveName, strcat('D:\Data\Henry\Surface autosave backups\',dirs{end-1},
 
 end
 
+function [ saveFile ] = create_surface_save_file( saveName, surfName, imaris )
+saveFile = matfile(saveName);
+
+%initialize fields
+saveFile.vertices = single([]);
+saveFile.triangles = int32([]);
+saveFile.normals = single([]);
+saveFile.timeIndex = int32([]);
+saveFile.numTriangles = int32([]);
+saveFile.numVertices = int32([]);
+saveFile.name = surfName;
+
+if (nargin == 3) %saving from stitched file, so get time info
+    dataset = imaris.GetDataSet;
+    numTP = dataset.GetSizeT;
+    timeCalibration = cell(numTP,1);
+    for i = 0:numTP-1
+       timeCalibration{1+i} = dataset.GetTimePoint(i); 
+    end
+    saveFile.timeCalibration = timeCalibration;
+end
+
+
+end
