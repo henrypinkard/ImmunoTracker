169af14ff095c8df92f614f59036ef8c72c11a80
Henry <henry.pinkard@gmail.com>
2019-07-25 20:39:50 -0700

    preparing for singularity

--- Pymaricumpiler/data_conversion.py
+++ Pymaricumpiler/data_conversion.py
@@ -4,29 +4,24 @@ import csv
 import numpy as np
 
 
-#download up to date spreadsheet listing experiments
-if 0 == os.system("rclone copy wallercloud:henry/lymphosight/LymphosightDatasets.csv ."):
-    print('downloading dataset list')
-    data_list = './LymphosightDatasets.csv'
-    print('data list downloaded')
-    imaris_dir = '/media/hugespace/henry/lymphosight/imaris_files'
-    raw_data_dir = '/media/hugespace/henry/lymphosight/raw_data/'
-    param_cache_dir = '/media/hugespace/henry/lymphosight/optimized_params/'
-else:
-    #on the berkeley cluster 
-    data_list = '/global/home/users/hbp/data/lymphosight/LymphosightDatasets.csv'
-    imaris_dir = '/global/home/users/hbp/data/lymphosight/imaris_files'
-    raw_data_dir = '/global/home/users/hbp/data/lymphosight/raw_data/'
-    param_cache_dir = '/global/home/users/hbp/data/lymphosight/optimized_params/'
+data_list = '/LymphosightDatasets.csv'
+
+#cosmos
+imaris_dir = '/media/hugespace/henry/lymphosight/imaris_files'
+raw_data_dir = '/media/hugespace/henry/lymphosight/raw_data/'
+param_cache_dir = '/media/hugespace/henry/lymphosight/optimized_params/'
+
+#Savio
+imaris_dir = '/global/home/users/hbp/data/lymphosight/imaris_files'
+raw_data_dir = '/global/home/users/hbp/data/lymphosight/raw_data/'
+param_cache_dir = '/global/home/users/hbp/data/lymphosight/optimized_params/'
+
 
 with open(data_list, 'r') as f:
     reader = csv.DictReader(f)
     experiments = list(reader)
     column_names = reader.fieldnames
 
-#remove data list if its beign redownloaded every time
-if data_list == './LymphosightDatasets.csv':
-    os.remove(data_list)
 
 
 def get_dataset_path(ID):
@@ -60,6 +55,6 @@ for ID in ids:
     convert(magellan_dir, position_registrations='optimize', input_filter_sigma=2,
             output_dir=imaris_dir, output_basename=ID, intra_stack_registration_channels=[1, 2, 3, 4, 5],
             timepoint_registration_channel=tp_ch, reverse_rank_filter=True, param_cache_dir=param_cache_dir,
-            num_time_points=ntp, suffix='2x2_ch05_noreg',
+            num_time_points=ntp, suffix='singularity_test',
             inter_stack_registration_channels=isr_ch, downsample_factor=2,
             stitch_regularization=0, stack=False, stitch=False, export=True)
