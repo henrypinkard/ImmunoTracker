fixed some stitching bugs by swapping around signs

Pymaricumpiler/pymaricumpiler.py
@@ -217,6 +217,8 @@ def convert(magellan_dir, position_registrations=None, register_timepoints=True,
 
         #Make all shifts relative to a timepoint in the middle? because the timelapses seem to stabilize by then
         t_p_zyx_fourier_translations = t_p_zyx_fourier_translations - t_p_zyx_fourier_translations[t_p_zyx_fourier_translations.shape[0] // 2, :, :]
+        #negate so it works properly with stitching code
+        t_p_zyx_fourier_translations[:, :, 0] = -t_p_zyx_fourier_translations[:, :, 0]
         #take median shift at each timepoint as the global shift
         t_zyx_global_shifts = np.round(np.mean(t_p_zyx_fourier_translations, axis=1)).astype(np.int)
         #include the residual as a starting point for the stitching
@@ -225,10 +227,6 @@ def convert(magellan_dir, position_registrations=None, register_timepoints=True,
         #pcache these values so they don't need to be recomputed
         np.savez('{}{}_optimized_params'.format(param_cache_dir, output_basename + '_time_reg'),
                  **{'t_zyx_global_shifts': t_zyx_global_shifts, 't_p_zyx_residual_shifts': t_p_zyx_residual_shifts})
-
-    # make z negative as the stitching code expects
-    t_p_zyx_residual_shifts[:, :, 0] = -t_p_zyx_residual_shifts[:, :, 0]
-
 
     ##### Stitching loop #####
     #default to 0 if not loading or computing
@@ -254,7 +252,8 @@ def convert(magellan_dir, position_registrations=None, register_timepoints=True,
             # get backgrounds from first time point
             backgrounds = estimate_background(t_p_zyxc_stacks[0], t_p_nonempty_pixels[0])
 
-        total_shifts = t_zyx_global_shifts[:, None,:] - t_p_zyx_residual_shifts
+        total_shifts = -t_zyx_global_shifts[:, None,:] - t_p_zyx_residual_shifts
+        #TODO: probably want to try flipping the sign of this and visualizing result
         t_p_z_positive_z_offset = total_shifts[:, :, 0] - np.min(total_shifts[:, :, 0])
         shifted_z_size = np.ptp(np.ravel(t_p_z_positive_z_offset)) + 1
 
@@ -323,7 +322,9 @@ def convert(magellan_dir, position_registrations=None, register_timepoints=True,
         return
 
     #make all global shifts positive
-    t_zyx_global_shifts -= np.round(np.min(t_zyx_global_shifts, axis=0))
+
+    t_zyx_global_shifts -= np.min(t_zyx_global_shifts, axis=0)
+    t_zyx_global_shifts = np.round(t_zyx_global_shifts).astype(np.int)
 
     #compute the size of teh stiched image accounting for movements in z
     stitched_image_size = [
@@ -335,8 +336,6 @@ def convert(magellan_dir, position_registrations=None, register_timepoints=True,
     #add in time point to timepoint registrations for the final imaris size
     imaris_size = np.array(stitched_image_size) + np.max(t_zyx_global_shifts, axis=0).astype(np.int) + \
                     np.max(t_p_zyx_translations[:, :, 0]).astype(np.int)
-
-
 
 
     output_basename = output_basename + suffix

Pymaricumpiler/utility.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import interpolate
 
 
-def anisotropic_x_corr_register_3D(volume1_xy, volume2_xy, volume1_z, volume2_z, max_shift=None):
+def anisotropic_x_corr_register_3D(volume1_xy, volume2_xy, volume1_z, volume2_z, max_shift=None, max_shift_z=10):
     """
     Cross correlation 3d register with different channels for XY and Z
     :param volume1:
@@ -12,6 +12,7 @@ def anisotropic_x_corr_register_3D(volume1_xy, volume2_xy, volume1_z, volume2_z,
     """
     if max_shift is None:
         max_shift = np.array(volume1_xy.shape) // 2
+        max_shift[0] = max_shift_z
     src_ft = np.fft.fftn(volume1_xy)
     target_ft = np.fft.fftn(volume2_xy)
     cross_corr = np.fft.ifftn((src_ft * target_ft.conj()))