0dc6e51e79825723b6155932cb70cad06e2977ae
Henry Pinkard <henry.pinkard@gmail.com>
2019-10-14 14:15:57 -0700

    fix time metadata on fusion datasets

--- registration/update_time_metadata.py
+++ registration/update_time_metadata.py
@@ -4,25 +4,53 @@ Rewrite time calibration to compensate for bug in imaricumpiler
 import h5py
 import numpy as np
 
-names = [ '21.ims', '24.ims', '25.ims', '27.ims', '28.ims', '31.ims', '34.ims', '36.ims', '38.ims', '39.ims', '44.ims',
-  '45.ims', '46.ims', '47.ims', '48-49_fusion_zoomin.ims', '48.ims', '49.ims', '49smoothed_but_no_corrections.ims',
-  '51.ims', '52.ims', '53.ims', '54.ims'];
+# names = [ '21.ims', '24.ims', '25.ims', '27.ims', '28.ims', '31.ims', '34.ims', '36.ims', '38.ims', '39.ims', '44.ims',
+#   '45.ims', '46.ims', '47.ims', '48-49_fusion_zoomin.ims', '48.ims', '49.ims', '49smoothed_but_no_corrections.ims',
+#   '51.ims', '52.ims', '53.ims', '54.ims'];
+#
+# for name in names:
+#     print(name)
+#     filename = '/Users/henrypinkard/Desktop/imaris_analysis/{}'.format(name)
+#     file = h5py.File(filename, mode='r+')
+#
+#     for key in file['DataSetInfo']['TimeInfo'].attrs.keys():
+#         if 'TimePoint' in key and 'Dataset' not in key and 'File' not in key:
+#             string = ''.join([letter.decode("utf-8") for letter in file['DataSetInfo']['TimeInfo'].attrs[key]])
+#
+#             split1 = string.split(':')
+#             h = int(split1[0][-1])
+#             m = int(split1[1]) % 60
+#             s = float(split1[2]) %60
+#             reformatted = '2018-06-01 {:02d}:{:02d}:{:.3f}'.format(h, m, s)
+#             # print(key + '\t' + string)
+#             # print(reformatted)
+#             file['DataSetInfo']['TimeInfo'].attrs.create(key, np.array([c.encode('utf-8') for c in reformatted]))
 
-for name in names:
-    print(name)
-    filename = '/Users/henrypinkard/Desktop/imaris_analysis/{}'.format(name)
-    file = h5py.File(filename, mode='r+')
 
-    for key in file['DataSetInfo']['TimeInfo'].attrs.keys():
-        if 'TimePoint' in key and 'Dataset' not in key and 'File' not in key:
-            string = ''.join([letter.decode("utf-8") for letter in file['DataSetInfo']['TimeInfo'].attrs[key]])
+source_name = '45.ims'
+name = '44-45_fusion_zoomin.ims'
+#add timestamps to fusion datasets
+filename = '/Users/henrypinkard/Desktop/imaris_analysis/{}'
+file = h5py.File(filename.format(name), mode='r+')
+source_file = h5py.File(filename.format(source_name), mode='r+')
 
+for key in file['DataSetInfo']['TimeInfo'].attrs.keys():
+    if 'TimePoint' in key and 'Dataset' not in key and 'File' not in key:
+        if key == 'TimePoint1':
+            string = ''.join([letter.decode("utf-8") for letter in source_file['DataSetInfo']['TimeInfo'].attrs[key]])
             split1 = string.split(':')
             h = int(split1[0][-1])
             m = int(split1[1]) % 60
             s = float(split1[2]) %60
             reformatted = '2018-06-01 {:02d}:{:02d}:{:.3f}'.format(h, m, s)
-            # print(key + '\t' + string)
-            # print(reformatted)
+            file['DataSetInfo']['TimeInfo'].attrs.create(key, np.array([c.encode('utf-8') for c in reformatted]))
+        else:
+            sourcekey = 'TimePoint{}'.format(int(key[9:]) - 1)
+            string = ''.join([letter.decode("utf-8") for letter in source_file['DataSetInfo']['TimeInfo'].attrs[sourcekey]])
+            split1 = string.split(':')
+            h = int(split1[0][-1])
+            m = int(split1[1]) % 60
+            s = float(split1[2]) %60
+            reformatted = '2018-06-01 {:02d}:{:02d}:{:.3f}'.format(h, m, s)
             file['DataSetInfo']['TimeInfo'].attrs.create(key, np.array([c.encode('utf-8') for c in reformatted]))
 
